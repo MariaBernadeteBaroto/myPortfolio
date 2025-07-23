d22c82b6e93317bb9c7ceba6e9296ffc99a86284
Maria Bernadete P. Baroto <92656206+MariaBernadeteBaroto@users.noreply.github.com>
2025-07-23 09:24:12 +0800

    edited description

--- src/components/homePage.tsx
+++ src/components/homePage.tsx
@@ -4,11 +4,14 @@ export default function HomePage() {
   return (
     <main className="min-h-screen bg-gradient-to-br from-purple-900 via-black to-black text-white">
       <section className="flex flex-col items-center justify-center min-h-screen min-w-screen text-center">
-        <h1 className="text-4xl md:text-6xl font-bold mb-4">hi, I'm Bernadete</h1>
-        <p className="text-lg md:text-xl text-gray-300 max-w-xl">
+        <div className="justify-left">
+        <h1 className="text-2xl md:text-4xl font-bold mb-4 font-serif">Hi there,</h1>
+        <h2 className="text-4xl md:text-6xl font-bold mb-4 font-sans">I'm Bernadete</h2>
+        </div>
+        <p className="text-lg md:text-xl text-gray-300 max-w-xl italic">
           When you get here, you probably got my resume.
           <br />
-          Now let me tell you a bit more detailed about myself.
+          Now let me tell you a bit more about myself.
         </p>
       </section>
     </main>
